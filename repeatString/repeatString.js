26960beaaa8122f5c6e7ed4aa6bf6a4ebb4b31a5
Chris <chrisray1997@gmail.com>
2020-08-25 15:42:23 -0400

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,7 +1,11 @@
-const repeatString = function(word, times) {
-    if (times < 0) return 'error'
-    let string = ''
-    
-}
 
-module.exports = repeatString
+var repeatString = function(word, times) {
+    if (times < 0) return 'ERROR'
+    let string = ''
+    for (let i = 0; i < times; i++) {
+      string += word
+    }
+    return string
+  }
+  
+  module.exports = repeatString
